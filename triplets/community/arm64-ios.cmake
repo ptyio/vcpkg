9f49e15d2dbb6396b5e966c758765e10e5da7fb2
Vitaly Petrov <v31337@gmail.com>
2021-12-25 22:51:22 +0300

    Allow release only builds on CI
    Disable qt icu dependency
    
    Temp fix libgcrypt
    
    Update arm64-osx.cmake
    
    Added chainload path to arm-linux triplet
    
    Update arm64-linux.cmake
    
    Update arm-linux.cmake
    
    Update arm64-linux.cmake
    
    Update arm-linux.cmake
    
    Update arm64-linux.cmake
    
    Revert some extra changes
    
    Update configure_qt.cmake
    
    Update vcpkg.json
    
    Update portfile.cmake
    
    Update arm-linux.cmake
    
    Update arm-linux.cmake
    
    Update arm64-linux.cmake
    
    Update arm64-linux.cmake
    
    Update arm-linux.cmake
    
    Update configure_qt.cmake
    
    Update vcpkg_configure_qmake.cmake
    
    Update vcpkg_configure_qmake.cmake
    
    Update portfile.cmake
    
    Update portfile.cmake
    
    Update portfile.cmake
    
    Update configure_qt.cmake
    
    Update configure_qt.cmake
    
    Experiments
    
    Experiments
    
    Experiments
    
    Experiments
    
    Experiments
    
    Experiments
    
    arm64-osx-full
    
    Added x86-linux triplet
    
    Added x86-linux support for qt5-base
    
    Rel only for new lib
    
    Support to build qt5-base fow Windows ARM
    
    Cleanup
    
    Fix arm64-win ptyqt cross
    
    Added qt5-websockets build support
    
    More fixes
    
    Rever automoc for ptyqt
    
    Update macos deployment targets
    
    Added freebsd target for qt5-base
    
    Added release-only settings for more triplets
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    More FreeBSD fixes
    
    Update prtqt
    
    armv6/armv7/arm64 linux
    
    aarch64 fixes
    
    fix triplet
    
    ios initial support
    
    iosfix

--- triplets/community/arm64-ios.cmake
+++ triplets/community/arm64-ios.cmake
@@ -2,3 +2,11 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
+
+set(VCPKG_BUILD_TYPE release)
+
+# todo find a way how to build all vcpkg libs with bitcode
+#set(XCODE_ATTRIBUTE_ENABLE_BITCODE ON)
+#set(ENABLE_BITCODE ON)
+#add_definitions(-DENABLE_BITCODE=ON)
+#set_property(GLOBAL PROPERTY XCODE_ATTRIBUTE_ENABLE_BITCODE ON)
